1a0ffd71c17ef1765f6bce873e7e2837f87457aa
jeremynoh <jeremynohile@gmail.com>
2022-12-14 19:25:59 +0100

    init 📱 + 🖥

--- app/app.tsx
+++ app/app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-color-literals */
 /**
  * Welcome to the main entry point of the app. In this file, we'll
  * be kicking off our app.
@@ -9,57 +10,47 @@
  * The app navigation resides in ./app/navigators, so head over there
  * if you're interested in adding screens and navigators.
  */
-import "./i18n"
-import "./utils/ignoreWarnings"
-import { useFonts } from "expo-font"
-import React from "react"
-import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
-import * as Linking from "expo-linking"
-import { useInitialRootStore } from "./models"
-import { AppNavigator, useNavigationPersistence } from "./navigators"
-import { ErrorBoundary } from "./screens/ErrorScreen/ErrorBoundary"
-import * as storage from "./utils/storage"
-import { customFontsToLoad } from "./theme"
-import { setupReactotron } from "./services/reactotron"
-import Config from "./config"
+// import "./i18n"
+// import "./utils/ignoreWarnings"
+// import { useFonts } from "expo-font"
 
+// import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
+// import * as Linking from "expo-linking"
+// import { useInitialRootStore } from "./models"
+// import { AppNavigator, useNavigationPersistence } from "./navigators"
+// import { ErrorBoundary } from "./screens/ErrorScreen/ErrorBoundary"
+// import * as storage from "./utils/storage"
+// import { customFontsToLoad } from "./theme"
+// import { setupReactotron } from "./services/reactotron"
+// import Config from "./config"
+
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import React, { useEffect, useCallback, useMemo, useRef } from "react"
+import { View, Text, StyleSheet, SafeAreaView, Button } from "react-native"
+import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet"
 // Set up Reactotron, which is a free desktop app for inspecting and debugging
 // React Native apps. Learn more here: https://github.com/infinitered/reactotron
-setupReactotron({
-  // clear the Reactotron window when the app loads/reloads
-  clearOnLoad: true,
-  // generally going to be localhost
-  host: "localhost",
-  // Reactotron can monitor AsyncStorage for you
-  useAsyncStorage: true,
-  // log the initial restored state from AsyncStorage
-  logInitialState: true,
-  // log out any snapshots as they happen (this is useful for debugging but slow)
-  logSnapshots: false,
-})
-
-export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
 
 // Web linking configuration
-const prefix = Linking.createURL("/")
-const config = {
-  screens: {
-    Login: {
-      path: "",
-    },
-    Welcome: "welcome",
-    Demo: {
-      screens: {
-        DemoShowroom: {
-          path: "showroom/:queryIndex?/:itemIndex?",
-        },
-        DemoDebug: "debug",
-        DemoPodcastList: "podcast",
-        DemoCommunity: "community",
-      },
-    },
-  },
-}
+// const prefix = Linking.createURL("/")
+// const config = {
+//   screens: {
+//     Login: {
+//       path: "",
+//     },
+//     Welcome: "welcome",
+//     Demo: {
+//       screens: {
+//         DemoShowroom: {
+//           path: "showroom/:queryIndex?/:itemIndex?",
+//         },
+//         DemoDebug: "debug",
+//         DemoPodcastList: "podcast",
+//         DemoCommunity: "community",
+//       },
+//     },
+//   },
+// }
 
 interface AppProps {
   hideSplashScreen: () => Promise<void>
@@ -70,23 +61,29 @@ interface AppProps {
  */
 function App(props: AppProps) {
   const { hideSplashScreen } = props
-  const {
-    initialNavigationState,
-    onNavigationStateChange,
-    isRestored: isNavigationStateRestored,
-  } = useNavigationPersistence(storage, NAVIGATION_PERSISTENCE_KEY)
 
-  const [areFontsLoaded] = useFonts(customFontsToLoad)
-
-  const { rehydrated } = useInitialRootStore(() => {
-    // This runs after the root store has been initialized and rehydrated.
-
-    // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to prevent flicker.
-    // Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
-    // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android Studio. Kill the app and launch it normally by tapping on the launcher icon. https://stackoverflow.com/a/69831106
-    // Note: (vanilla iOS) You might notice the splash-screen logo change size. This happens in debug/development mode. Try building the app for release.
+  useEffect(() => {
     setTimeout(hideSplashScreen, 500)
-  })
+  }, [])
+
+  // const { hideSplashScreen } = props
+  // const {
+  //   initialNavigationState,
+  //   onNavigationStateChange,
+  //   isRestored: isNavigationStateRestored,
+  // } = useNavigationPersistence(storage, NAVIGATION_PERSISTENCE_KEY)
+
+  // const [areFontsLoaded] = useFonts(customFontsToLoad)
+
+  // const { rehydrated } = useInitialRootStore(() => {
+  //   // This runs after the root store has been initialized and rehydrated.
+
+  //   // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to prevent flicker.
+  //   // Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
+  //   // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android Studio. Kill the app and launch it normally by tapping on the launcher icon. https://stackoverflow.com/a/69831106
+  //   // Note: (vanilla iOS) You might notice the splash-screen logo change size. This happens in debug/development mode. Try building the app for release.
+  //   setTimeout(hideSplashScreen, 500)
+  // })
 
   // Before we show the app, we have to wait for our state to be ready.
   // In the meantime, don't render anything. This will be the background
@@ -94,25 +91,72 @@ function App(props: AppProps) {
   // In iOS: application:didFinishLaunchingWithOptions:
   // In Android: https://stackoverflow.com/a/45838109/204044
   // You can replace with your own loading component if you wish.
-  if (!rehydrated || !isNavigationStateRestored || !areFontsLoaded) return null
+  // if (!rehydrated || !isNavigationStateRestored || !areFontsLoaded) return null
 
-  const linking = {
-    prefixes: [prefix],
-    config,
-  }
+  // const linking = {
+  //   prefixes: [prefix],
+  //   config,
+  // }
+
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null)
+
+  // variables
+  const snapPoints = useMemo(() => ["25%", "50%", "100%"], [])
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present()
+  }, [])
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index)
+  }, [])
 
   // otherwise, we're ready to render the app
   return (
-    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      <ErrorBoundary catchErrors={Config.catchErrors}>
-        <AppNavigator
-          linking={linking}
-          initialState={initialNavigationState}
-          onStateChange={onNavigationStateChange}
-        />
-      </ErrorBoundary>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{}}>
+        <Text>Jeje je ssias pas quoi fiare</Text>
+        <Button onPress={handlePresentModalPress} title="Present Modal" color="black" />
+      </SafeAreaView>
+
+      <BottomSheetModalProvider>
+        <View style={styles.container}>
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            index={1}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}
+          >
+            <View style={styles.contentContainer}>
+              <Text>Awesome 🎉</Text>
+            </View>
+          </BottomSheetModal>
+        </View>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+    // <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+    //   <ErrorBoundary catchErrors={Config.catchErrors}>
+    //     <AppNavigator
+    //       linking={linking}
+    //       initialState={initialNavigationState}
+    //       onStateChange={onNavigationStateChange}
+    //     />
+    //   </ErrorBoundary>
+    // </SafeAreaProvider>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "grey",
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  contentContainer: {
+    alignItems: "center",
+    flex: 1,
+  },
+})
